08e3661bf22c8328e3a53308e5c69429f707cb11
NicholasTourony <natourony@quinnipiac.edu>
2022-10-31 19:00:57 -0400

    Added mute and trying other website support

--- popup.js
+++ popup.js
@@ -2,6 +2,8 @@
 // volume up and down buttons to add event listener
 let volumeDown = document.getElementById("VolumeDown");
 let volumeUp = document.getElementById("VolumeUp");
+let mute = document.getElementById("mute");
+let muted = false;
 
 // set the text of the popup to the current volume
 chrome.storage.sync.get("volume", (data) => {
@@ -21,6 +23,45 @@ volumeUp.addEventListener("click", async () => {
     changeVolume(10);
 });
 
+mute.addEventListener("click", async () => {
+    console.log('muting volume');
+    muteVolume();
+});
+
+function muteVolume()
+{
+    if (!muted)
+    {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            const activeTab = tabs[0];
+
+        chrome.tabs.sendMessage(activeTab.id, {
+            message: 'set_volume_level',
+            value: 0
+          });
+        });
+        muted = true;
+        mute.textContent = "unmute";
+    }
+    else
+    {
+        chrome.storage.sync.get("volume", (data) => {
+            volume = data.volume;
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            const activeTab = tabs[0];
+
+        chrome.tabs.sendMessage(activeTab.id, {
+            message: 'set_volume_level',
+            value: volume / 100.0
+          });
+        });
+    });
+    muted = false;
+    mute.textContent = "mute";
+    }
+}
+
 // take the change in volume and calculate a new volume
 // store this volume and tell the content script to change the volume
 function changeVolume(volume)
@@ -30,6 +71,10 @@ function changeVolume(volume)
         if (volume < 0)
         {
             volume = 0;
+        }
+        else if (volume > 400)
+        {
+            volume = 400;
         }
         chrome.storage.sync.set({volume}, function() {
         console.log("Volume set to: " + volume);
@@ -45,4 +90,6 @@ function changeVolume(volume)
           });
         });
     });
+    muted = false;
+    mute.textContent = "mute";
 }
